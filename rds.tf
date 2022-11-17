bfba572a5594bb23bea14dc2ad014c57b430ca79
Miskeen <hussainajhar8@gmail.com>
2022-11-17 21:58:43 +0000

    Operation 3 Tier Architecture successful

---
+++ rds.tf
@@ -0,0 +1,28 @@
+# Creating RDS Instance
+resource "aws_db_instance" "rds_instance" {
+  depends_on = [module.vpc]
+  allocated_storage      = var.allocated_storage
+  engine                 = var.engine
+  engine_version         = var.engine_version
+  instance_class         = var.instance_class
+  multi_az               = false
+  name                   = var.db_name
+  username               = var.username
+  password               = var.password
+  skip_final_snapshot    = true
+  db_subnet_group_name = aws_db_subnet_group.my_rds.id
+  vpc_security_group_ids = [module.public_bastion_sg.this_security_group_id, module.private_sg.this_security_group_id]
+  #vpc_security_group_ids = [module.private_sg.this_security_group_id]
+  availability_zone = var.vpc_availability_zones[0]
+  
+}
+
+resource "aws_db_subnet_group" "my_rds" {
+  name       = var.db_subnet_group_name
+  subnet_ids = module.vpc.database_subnets
+
+  tags = {
+    Name = "My DB subnet group"
+  }
+}
+
